bb485e38e5a9cdea7cc673069663f7126d2b02a9
adt-hgkim <hgkim@gnmpm.com>
2023-08-11 12:56:25 +0900

    fix: redirect with 307

--- src/routes/[key]/+page.ts
+++ src/routes/[key]/+page.ts
@@ -2,9 +2,9 @@ import { redirect } from '@sveltejs/kit'
 import type { PageLoad } from './$types'
 
 export const load: PageLoad = async ({ params, fetch }) => {
-	const { slug } = params
-	const response = await fetch(`/api/v1/shortenUrl?key=${slug}`)
+	const { key } = params
+	const response = await fetch(`/api/v1/shortenUrl?key=${key}`)
 	const body = await response.json()
-	if (body.ok) throw redirect(301, '/?data=' + body.message)
-	else throw redirect(301, '/')
+	if (body.ok) throw redirect(307, '/?data=' + body.message)
+	else throw redirect(307, '/')
 }

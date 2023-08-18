71c67e40c7737a349e4c3dd583e83ca9af9d7018
adt-hgkim <hgkim@gnmpm.com>
2023-08-18 21:23:04 +0900

    feat: linebreaks

--- src/lib/components/Textarea.svelte
+++ src/lib/components/Textarea.svelte
@@ -7,21 +7,27 @@
 {#if !readonly}
 	<div class="flex flex-col w-full">
 		<label class="pl-0.5 font-medium">{key}</label>
-		<span contenteditable="true" autocomplete="off" role="textbox" bind:innerText={value} />
+		<div
+			class="textarea"
+			contenteditable="true"
+			autocomplete="off"
+			role="textbox"
+			bind:innerText={value}
+		/>
 	</div>
 {:else}
 	<div class="flex flex-col w-full">
 		<label class="pl-0.5 font-medium">{key}</label>
-		<span contenteditable="false" autocomplete="off">{value}</span>
+		<div class="textarea" contenteditable="false" autocomplete="off" bind:innerText={value} />
 	</div>
 {/if}
 
 <style>
-	span {
+	.textarea {
 		@apply border border-black border-solid rounded p-2 bg-transparent w-full text-sm font-light min-h-[2.5rem];
 	}
 
-	span[contenteditable='false'] {
+	.textarea[contenteditable='false'] {
 		@apply bg-black/5 cursor-not-allowed;
 	}
 </style>
